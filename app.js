24e1c7284d7e52f7cdb56229d3c1b96f79742886
Alexandru ISTRATII <aistratii@pentalog.com>
2019-05-02 12:52:40 +0300

    Calculate conversion according to rates fetched from the server

--- app.js
+++ app.js
@@ -2,13 +2,30 @@ new Vue({
     el: "#app",
 
     data: {
-        currencies: {
-
-        }
+        currencies: {},
+        amount: 0,
+        from: "EUR",
+        to: "USD",
+        result: 0,
+        loading: false
     },
 
     mounted() {
         this.getCurrencies();
+    },
+
+    computed: {
+        formattedCurrencies() {
+            return Object.values(this.currencies);
+        },
+
+        calculateResult() {
+            return (Number(this.amount) * this.result).toFixed(2);
+        },
+
+        disabled() {
+            return this.amount === 0 || !this.amount || this.loading;
+        }
     },
 
     methods: {
@@ -20,17 +37,35 @@ new Vue({
                 return;
             }
 
-            axios.get("http://localhost:8088/api/currencies",{/*this is request data*/}, {
-                headers: {
-                    "Access-Control-Allow-Origin": "*"
-                }
-            })
+            axios.get("http://localhost:8088/api/currencies",{/*this is request data*/}, { headers: {"Access-Control-Allow-Origin": "*"}})
             .then(response => {
                 console.log(response.data);
                 this.currencies = response.data.results;
                 localStorage.setItem("currencies", JSON.stringify(response.data.results));
             });
 
+        },
+
+        convertCurrency(){
+            const key = `${this.from}_${this.to}`;
+            this.loading = true;
+
+            axios.get(`http://localhost:8088/api/currencies/convert?q=${key}`, {/*this is request data*/}, { headers: {"Access-Control-Allow-Origin": "*"}})
+            .then(response => {
+                console.log(response.data.results[key]);
+                
+                this.result = response.data.results[key].val;
+                this.loading = false;
+            })
+        }
+    },
+
+    watch: {
+        from() {
+            this.result = 0; 
+        },
+        to() {
+            this.result = 0;
         }
     }
 });
