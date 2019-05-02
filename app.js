47652c2e764f620a215c9237d7001359924580b4
Alexandru ISTRATII <aistratii@pentalog.com>
2019-05-02 13:41:08 +0300

    Component and custom tag

--- app.js
+++ app.js
@@ -1,71 +1,19 @@
+Vue.component("card", {
+    template:`
+        <div class="card">
+            <div class="card-body">
+                <h3 class="card-title">
+                    Vuejs fundamentals
+                </h3>
+
+                <div class="card-text">
+                    asdkjnsalkglklkgn kln glkn skndf lk'nsdlk 'nfklns dkfn s'
+                </div>
+            </div>
+        </div>
+    `
+});
+
 new Vue({
-    el: "#app",
-
-    data: {
-        currencies: {},
-        amount: 0,
-        from: "EUR",
-        to: "USD",
-        result: 0,
-        loading: false
-    },
-
-    mounted() {
-        this.getCurrencies();
-    },
-
-    computed: {
-        formattedCurrencies() {
-            return Object.values(this.currencies);
-        },
-
-        calculateResult() {
-            return (Number(this.amount) * this.result).toFixed(2);
-        },
-
-        disabled() {
-            return this.amount === 0 || !this.amount || this.loading;
-        }
-    },
-
-    methods: {
-        getCurrencies(){
-            const currencies = localStorage.getItem("currencies");
-
-            if(currencies){
-                this.currencies = JSON.parse(currencies);
-                return;
-            }
-
-            axios.get("http://localhost:8088/api/currencies",{/*this is request data*/}, { headers: {"Access-Control-Allow-Origin": "*"}})
-            .then(response => {
-                console.log(response.data);
-                this.currencies = response.data.results;
-                localStorage.setItem("currencies", JSON.stringify(response.data.results));
-            });
-
-        },
-
-        convertCurrency(){
-            const key = `${this.from}_${this.to}`;
-            this.loading = true;
-
-            axios.get(`http://localhost:8088/api/currencies/convert?q=${key}`, {/*this is request data*/}, { headers: {"Access-Control-Allow-Origin": "*"}})
-            .then(response => {
-                console.log(response.data.results[key]);
-                
-                this.result = response.data.results[key].val;
-                this.loading = false;
-            })
-        }
-    },
-
-    watch: {
-        from() {
-            this.result = 0; 
-        },
-        to() {
-            this.result = 0;
-        }
-    }
+    el: "#app"
 });
