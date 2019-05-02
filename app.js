1c7fb4c4f85b99a8bb578a1a5124aca5b9f0c6af
Alexandru ISTRATII <aistratii@pentalog.com>
2019-05-02 14:07:16 +0300

    Custom component attributes with loop and json

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 Vue.component("card", {
-    template:`
+    props: ["title", "content"],
+    template: `
         <div class="card">
             <div class="card-body">
                 <h3 class="card-title">
-                    Vuejs fundamentals
+                    {{ title }}
                 </h3>
 
                 <div class="card-text">
-                    asdkjnsalkglklkgn kln glkn skndf lk'nsdlk 'nfklns dkfn s'
+                    {{ content }}
                 </div>
             </div>
         </div>
@@ -15,5 +16,30 @@ Vue.component("card", {
 });
 
 new Vue({
-    el: "#app"
+    el: "#app",
+    data: {
+        articles: [
+            {
+            title: "sony, în româ",
+            content: `ste capitala și cel mai mare oraș din Slovacia și are o populație de aproximativ 430.000 locuitori.[1] Bratislava este situată pe Dunăre, aproape de granițele Slovaciei cu Austria și Ungaria și destul de aproape de granița cu Republica Cehă. Se află la doar 60 de k
+            m de Viena.[2] Munții Carpați încep pe teritoriul orașului Malé Karpaty („Carpații Mici”).
+            Bratislava este sediul președinției, parlamentului și guvernului slovac. În oraș se 
+            află câteva universități, muzee, teatre și`
+            },
+            {
+               title: "lovac poart",
+               content: "documentară a localității (sub numele de Brezalauspurc, „cetatea lui Brezalau”) este făcută de Analele de la Salzburg, în care sunt descrise Bătăliile de la Pojon, între bavarezi și unguri, duse în afara zidurilor Castelului Bratislava, în 907.[4] Castelul și-a primit probabil numele de la Predslav, al treilea fiu al regelui Svätopluk I, sau poate de la nobilul local Braslav.[5] Acest nume vechi reapare sub forma Braslava sau Preslava, pe monezi emise de regele Ștefan I al Ungariei, datând din jurul anului 1000.[6] Mai târziu în Evul Mediu, a ajuns la forma finală a "
+            },
+            {
+                title: "Prešporok",
+                content: `osony înainte de secolul al XIX-lea). Numele maghiar, încă folosit de vorbitorii acestei limbi, este posibil să fie derivat de la Božaň, conducător din secolul al XI-lea al Castelului Bratislava.[8] Pe lângă aceste nume, în documente din epoca Renașterii orașul a fost numit Istropolis, „Orașul Dunării” în greaca veche.
+
+                Numele actual, Bratislava, datează din 1837, când eruditul slavist Pavel Jozef Šafárik a reconstruit o variantă a lui (Břetislaw),[9] d`
+            },
+            {
+                title: "mai vec",
+                content: "(Břetislaw),[9] din nume mai vechi, considerând că erau derivate din numele conducătorului Boemiei Bretislav I. Numele a fost folosit inițial de către membrii mișcării naționale slovace în 1844, ca Bratislav.[9] După primul război mondial, unii reprezentanți non-slovaci ai orașului au încercat să-l redenumească „Wilson City”, în onoarea președintelui american Woodrow Wilson, și pentru a evita încorporarea acestuia în Cehoslovacia. Propunerea a fost respinsă, și numele oficial al orașului a devenit Bratislava în martie 1919, după ce orașul a ajuns să f"
+            }
+        ]
+    }
 });
